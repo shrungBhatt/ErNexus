6ba5d88eff9c0ea6e47afa56380ffb9ccbc9662e
Shrung Bhatt <huntjigsaw97@gmail.com>
2017-04-06 19:24:52 +0530

    6/4/17

--- app/src/main/java/com/example/andorid/ersnexus/util/BackgroundDbConnector.java
+++ app/src/main/java/com/example/andorid/ersnexus/util/BackgroundDbConnector.java
@@ -35,190 +35,197 @@ public class BackgroundDbConnector extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground (String... params) {
 
+        String URL = "http://192.168.2.3/ersnexus/";
+
         //The first parameter of the background task method.
         String type = params[0];
         mType = type;
 
         //Url for login page php file.
-        String loginUrl = "http://192.168.2.3/ersnexus/login.php";
+        String loginUrl = URL + "login.php";
 
         //Url for register page php file.
-        String registerUrl = "http://192.168.2.3/ersnexus/register.php";
+        String registerUrl = URL + "register.php";
 
-        String enrollmentNumberUrl = "http://192.168.2.3/ersnexus/enrollmentnumber.php";
+        String enrollmentNumberUrl = URL + "enrollmentnumber.php";
+
+        String attendanceUrl = URL + "attendance.php";
 
         //It is an login call.
-        if (type.equals("login")) {
-            try {
-                //Fetch the username and password from the background method call.
-                String username = params[1];
-                String password = params[2];
+        switch (type) {
+            case "login":
+                try {
+                    //Fetch the username and password from the background method call.
+                    String username = params[1];
+                    String password = params[2];
 
-                //Creating a URL.
-                URL url = new URL(loginUrl);
-                //Connecting to the URL.
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                //Setting request method POST.
-                httpURLConnection.setRequestMethod("POST");
-                //This connection include Input and output interaction.
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.setDoInput(true);
+                    //Creating a URL.
+                    URL url = new URL(loginUrl);
+                    //Connecting to the URL.
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    //Setting request method POST.
+                    httpURLConnection.setRequestMethod("POST");
+                    //This connection include Input and output interaction.
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
 
-                //Creating the outputStream
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                //Writing in the outputStream.
-                BufferedWriter bufferedWriter = new BufferedWriter(new
-                        OutputStreamWriter(outputStream, "UTF-8"));
+                    //Creating the outputStream
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    //Writing in the outputStream.
+                    BufferedWriter bufferedWriter = new BufferedWriter(new
+                            OutputStreamWriter(outputStream, "UTF-8"));
 
-                //This is for connecting the variables in the app and in the php file.
-                String postData = URLEncoder.encode("username", "UTF-8") + "=" +//$_POST["username"]
-                        URLEncoder.encode(username, "UTF-8") + "&" +
-                        URLEncoder.encode("password", "UTF-8") + "=" +//$_POST["password"]
-                        URLEncoder.encode(password, "UTF-8");
+                    //This is for connecting the variables in the app and in the php file.
+                    String postData = URLEncoder.encode("username", "UTF-8") + "=" +//$_POST["username"]
+                            URLEncoder.encode(username, "UTF-8") + "&" +
+                            URLEncoder.encode("password", "UTF-8") + "=" +//$_POST["password"]
+                            URLEncoder.encode(password, "UTF-8");
 
-                //Feeding the data.
-                bufferedWriter.write(postData);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                outputStream.close();
+                    //Feeding the data.
+                    bufferedWriter.write(postData);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
 
-                //Creating an inputStream to fetch the results.
-                InputStream inputStream = httpURLConnection.getInputStream();
+                    //Creating an inputStream to fetch the results.
+                    InputStream inputStream = httpURLConnection.getInputStream();
 
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-                        inputStream, "iso-8859-1"));
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                            inputStream, "iso-8859-1"));
 
-                //Getting the results
-                String result = "";
-                String line = "";
-                while ((line = bufferedReader.readLine()) != null) {
-                    result += line;
+                    //Getting the results
+                    String result = "";
+                    String line = "";
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result += line;
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    //Returning the results
+                    return result;
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                bufferedReader.close();
-                inputStream.close();
-                httpURLConnection.disconnect();
-                //Returning the results
-                return result;
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else if (type.equals("register")) {//It is an register POST call.
-            try {
-                //Fetching the values to be registered.
-                String enrollmentnumber = params[1];
-                String username = params[2];
-                String fullname = params[3];
-                String password = params[4];
-                String emailid = params[5];
-                String dob = params[6];
+                break;
+            case "register": //It is an register POST call.
+                try {
+                    //Fetching the values to be registered.
+                    String enrollmentnumber = params[1];
+                    String username = params[2];
+                    String fullname = params[3];
+                    String password = params[4];
+                    String emailid = params[5];
+                    String dob = params[6];
 
 
-                URL url = new URL(registerUrl);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.setDoInput(true);
+                    URL url = new URL(registerUrl);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
 
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new
-                        OutputStreamWriter(outputStream, "UTF-8"));
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    BufferedWriter bufferedWriter = new BufferedWriter(new
+                            OutputStreamWriter(outputStream, "UTF-8"));
 
-                String postData = URLEncoder.encode("enrollmentnumber", "UTF-8") + "=" +//$_POST["enrollmentnumber"]
-                        URLEncoder.encode(enrollmentnumber, "UTF-8") +
-                        "&" +
-                        URLEncoder.encode("username", "UTF-8") + "=" +//$_POST["username"]
-                        URLEncoder.encode(username, "UTF-8") +
-                        "&" +
-                        URLEncoder.encode("fullname", "UTF-8") + "=" +//$_POST["fullname"]
-                        URLEncoder.encode(fullname, "UTF-8") +
-                        "&" +
-                        URLEncoder.encode("password", "UTF-8") + "=" +//$_POST["password"]
-                        URLEncoder.encode(password, "UTF-8") +
-                        "&" +
-                        URLEncoder.encode("emailid", "UTF-8") + "=" +//$_POST["emailid"]
-                        URLEncoder.encode(emailid, "UTF-8") +
-                        "&" +
-                        URLEncoder.encode("dob", "UTF-8") + "=" +//$_POST["dob"]
-                        URLEncoder.encode(dob, "UTF-8");
+                    String postData = URLEncoder.encode("enrollmentnumber", "UTF-8") + "=" +//$_POST["enrollmentnumber"]
+                            URLEncoder.encode(enrollmentnumber, "UTF-8") +
+                            "&" +
+                            URLEncoder.encode("username", "UTF-8") + "=" +//$_POST["username"]
+                            URLEncoder.encode(username, "UTF-8") +
+                            "&" +
+                            URLEncoder.encode("fullname", "UTF-8") + "=" +//$_POST["fullname"]
+                            URLEncoder.encode(fullname, "UTF-8") +
+                            "&" +
+                            URLEncoder.encode("password", "UTF-8") + "=" +//$_POST["password"]
+                            URLEncoder.encode(password, "UTF-8") +
+                            "&" +
+                            URLEncoder.encode("emailid", "UTF-8") + "=" +//$_POST["emailid"]
+                            URLEncoder.encode(emailid, "UTF-8") +
+                            "&" +
+                            URLEncoder.encode("dob", "UTF-8") + "=" +//$_POST["dob"]
+                            URLEncoder.encode(dob, "UTF-8");
 
-                bufferedWriter.write(postData);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                outputStream.close();
+                    bufferedWriter.write(postData);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
 
-                InputStream inputStream = httpURLConnection.getInputStream();
+                    InputStream inputStream = httpURLConnection.getInputStream();
 
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-                        inputStream, "iso-8859-1"));
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                            inputStream, "iso-8859-1"));
 
-                String result = "";
-                String line = "";
-                while ((line = bufferedReader.readLine()) != null) {
-                    result += line;
+                    String result = "";
+                    String line = "";
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result += line;
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    return result;
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                bufferedReader.close();
-                inputStream.close();
-                httpURLConnection.disconnect();
-                return result;
+                break;
+            case "enrollmentnumber":
+                try {
+                    //Fetch the username from the background method call.
+                    String username = params[1];
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }else if(type.equals("enrollmentnumber")){
-            try {
-                //Fetch the username from the background method call.
-                String username = params[1];
+                    //Creating a URL.
+                    URL url = new URL(enrollmentNumberUrl);
+                    //Connecting to the URL.
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    //Setting request method POST.
+                    httpURLConnection.setRequestMethod("POST");
+                    //This connection include Input and output interaction.
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
 
-                //Creating a URL.
-                URL url = new URL(enrollmentNumberUrl);
-                //Connecting to the URL.
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                //Setting request method POST.
-                httpURLConnection.setRequestMethod("POST");
-                //This connection include Input and output interaction.
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.setDoInput(true);
+                    //Creating the outputStream
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    //Writing in the outputStream.
+                    BufferedWriter bufferedWriter = new BufferedWriter(new
+                            OutputStreamWriter(outputStream, "UTF-8"));
 
-                //Creating the outputStream
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                //Writing in the outputStream.
-                BufferedWriter bufferedWriter = new BufferedWriter(new
-                        OutputStreamWriter(outputStream, "UTF-8"));
+                    //This is for connecting the variables in the app and in the php file.
+                    String postData = URLEncoder.encode("username", "UTF-8") + "=" +//$_POST["username"]
+                            URLEncoder.encode(username, "UTF-8");
 
-                //This is for connecting the variables in the app and in the php file.
-                String postData = URLEncoder.encode("username", "UTF-8") + "=" +//$_POST["username"]
-                        URLEncoder.encode(username, "UTF-8");
+                    //Feeding the data.
+                    bufferedWriter.write(postData);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
 
-                //Feeding the data.
-                bufferedWriter.write(postData);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                outputStream.close();
+                    //Creating an inputStream to fetch the results.
+                    InputStream inputStream = httpURLConnection.getInputStream();
 
-                //Creating an inputStream to fetch the results.
-                InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                            inputStream, "iso-8859-1"));
 
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-                        inputStream, "iso-8859-1"));
+                    //Getting the results
+                    String result = "";
+                    String line = "";
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result += line;
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    //Returning the results
+                    SharedPreferences.setStoredErno(mContext, result);
+                    return result;
 
-                //Getting the results
-                String result = "";
-                String line = "";
-                while ((line = bufferedReader.readLine()) != null) {
-                    result += line;
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                bufferedReader.close();
-                inputStream.close();
-                httpURLConnection.disconnect();
-                //Returning the results
-                SharedPreferences.setStoredErno(mContext,result);
-                return result;
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+                break;
         }
         return null;
     }
