e87aec0de18ef5c0fdbd3bb0de6aa2ada23abf89
Shrung <shrungbhatt@gmail.com>
2017-07-31 14:31:30 +0530

    Created singleFragmentActivity

--- app/src/main/java/com/example/andorid/ersnexus/userprofile/tabs/assignment/Assignments.java
+++ app/src/main/java/com/example/andorid/ersnexus/userprofile/tabs/assignment/Assignments.java
@@ -32,6 +32,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 //This class is the Assignment Tab.
@@ -207,6 +208,7 @@ public class Assignments extends Fragment {
             mAssignmentData = items;
             if (mAssignmentData != null) {
                 AssignmentData.setAssignments(mAssignmentData);
+                Collections.reverse(mAssignmentData);
                 mAssignmentRecyclerView.setAdapter(new AssignmentAdapter(mAssignmentData));
             }
         }
